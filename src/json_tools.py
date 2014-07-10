1d8b27ff73bd56488f1f45b3d820ddd0e5b1edb4
Pablo N Dalmasso <pablodalmasso@gmail.com>
2014-07-10 11:59:58 -0300

    "se corrigen detalles del settings y se mejora la lectura de los json"

--- src/json_tools.py
+++ src/json_tools.py
@@ -4,6 +4,6 @@ import json
 
 def load(patch):
     f = open(patch)
-    datos = f.read()
+    datos = f.read().decode()
     data = json.loads(datos)
     return data
--- src/settings.py
+++ src/settings.py
@@ -1,63 +1,13 @@
 MODELS = [
-    # {
-    #     'name': '',
-    #     'patch': '',
-    #     'foreignkey': [
-    #         {
-    #             'model_to': '',
-    #             'related_name': '',
-    #             'col_from': '',
-    #             'col_to': '',
-    #         },
-    #     ]
-    # },
     {
-        'name': 'cliente',
-        'patch': '/home/pdalmasso/Descargas/cliente.json',
-        'foreignkey': []
-    },
-    {
-        'name': 'juzgado',
-        'patch': '/home/pdalmasso/Descargas/juzgado.json',
-        'foreignkey': []
-    },
-    {
-        'name': 'jurisdiccion',
-        'patch': '/home/pdalmasso/Descargas/jurisdiccion.json',
-        'foreignkey': []
-    },
-    {
-        'name': 'estado',
-        'patch': '/home/pdalmasso/Descargas/estado.json',
-        'foreignkey': []
-    },
-    {
-        'name': 'casos',
-        'patch': '/home/pdalmasso/Descargas/caso.json',
+        'name': '',
+        'patch': '',
         'foreignkey': [
             {
-                'model_to': 'cliente',
-                'related_name': 'cliente',
-                'col_from': 'c_id_cliente',
-                'col_to': 'idcliente',
-            },
-            {
-                'model_to': 'juzgado',
-                'related_name': 'juzgado',
-                'col_from': 'c_id_juzgado',
-                'col_to': 'idjuzgado',
-            },
-            {
-                'model_to': 'jurisdiccion',
-                'related_name': 'jurisdiccion',
-                'col_from': 'c_id_jurisdiccion',
-                'col_to': 'idjurisdiccion',
-            },
-            {
-                'model_to': 'estado',
-                'related_name': 'estado',
-                'col_from': 'c_id_estado',
-                'col_to': 'idestado',
+                'model_to': '',
+                'related_name': '',
+                'col_from': '',
+                'col_to': '',
             },
         ]
     },
